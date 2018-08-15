3d47c86df28753927ab43eeff6433dc20737886a
Eragon <450614754@qq.com>
2018-08-15 23:13:54 +0800

    Initial commit

---
+++ Source/XD_ItemSystem/Private/XD_ItemSystemBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "XD_ItemSystemBPLibrary.h"
+#include "XD_ItemSystem.h"
+
+UXD_ItemSystemBPLibrary::UXD_ItemSystemBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UXD_ItemSystemBPLibrary::XD_ItemSystemSampleFunction(float Param)
+{
+	return -1;
+}
+
